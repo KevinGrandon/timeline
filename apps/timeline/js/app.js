8610fc3a278168786dba769800b84cd0bd6462c1
Kevin Grandon <kevingrandon@yahoo.com>
2015-02-10 18:39:26 +0100

    Smooth scroll to now position.

--- apps/timeline/js/app.js
+++ apps/timeline/js/app.js
@@ -3,6 +3,7 @@
 (function(exports) {
 
   function App() {
+    window.addEventListener('hashchange', this);
   }
 
   App.prototype = {
@@ -12,12 +13,21 @@
       var currentTime = new CurrentTimeItem(timeContainer);
 
       setTimeout(() => {
+        // Scroll to 'now' to hide future items.
+        var nowTop = timeContainer.getBoundingClientRect().top;
+        document.documentElement.scrollTop = nowTop;
+      });
+    },
 
-
-      // Scroll to 'now' to hide future items.
-      var nowTop = timeContainer.getBoundingClientRect().top;
-      document.documentElement.scrollTop = nowTop;
-    });
+    handleEvent: function(e) {
+      switch(e.type) {
+        case 'hashchange':
+          var timeContainer = document.getElementById('now');
+          var nowTop = timeContainer.getBoundingClientRect().top;
+          var scrollTo = document.documentElement.scrollTop + nowTop;
+          document.documentElement.scrollTo({left: 0, top: scrollTo, behavior: 'smooth'});
+          break;
+      }
     }
 
   };
