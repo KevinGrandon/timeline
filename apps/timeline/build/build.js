5d4ddd771f568d2bf1297eef8f61205e06d1a023
Kevin Grandon <kevingrandon@yahoo.com>
2015-02-10 17:37:13 +0100

    Timeline build fixes.

--- apps/timeline/build/build.js
+++ apps/timeline/build/build.js
@@ -2,25 +2,15 @@
 
 /* global require, exports */
 var utils = require('utils');
-var manager = require('homescreen-manager');
-var svoperapps = require('./homescreen-svoperapps');
 
-var VerticalHomeAppBuilder = function() {
+var TimelineBuilder = function() {
 };
 
-VerticalHomeAppBuilder.prototype.execute = function(options) {
-  var homescreen = manager.getHomescreen(options);
+TimelineBuilder.prototype.execute = function(options) {
 
-  var stageDir = utils.getFile(options.STAGE_APP_DIR);
-  var configFile = utils.getFile(stageDir.path, 'js', 'init.json');
-  utils.writeContent(configFile, JSON.stringify(homescreen));
-
-  if (options.VARIANT_PATH) {
-    svoperapps.execute(options, homescreen, stageDir);
-  }
 };
 
 exports.execute = function(options) {
   utils.copyToStage(options);
-  (new VerticalHomeAppBuilder()).execute(options);
+  (new TimelineBuilder()).execute(options);
 };
