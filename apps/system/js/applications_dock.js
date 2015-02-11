90b4800c25e98648e807cf50a594f80c9eb2a520
Kevin Grandon <kevingrandon@yahoo.com>
2015-02-11 17:24:47 +0100

    Generate a select menu for icon customization

--- apps/system/js/applications_dock.js
+++ apps/system/js/applications_dock.js
@@ -1,5 +1,10 @@
 (function() {
 
+  var iconConfig = [
+    ['app://communications.gaiamobile.org/manifest.webapp', 'contacts'],
+    ['app://search.gaiamobile.org/manifest.webapp']
+  ];
+
   function ApplicationDock() {
     this.map = new WeakMap();
     this.icons = [
@@ -15,6 +20,8 @@
 
     this.icons.forEach(icon => {
       icon.addEventListener('click', this)
+      icon.addEventListener('touchstart', this)
+      icon.addEventListener('touchend', this)
     });
   }
 
@@ -24,13 +31,8 @@
      * Renders the system-wide dock once applications is ready.
      */
     render: function() {
-      var manifests = [
-        ['app://communications.gaiamobile.org/manifest.webapp', 'contacts'],
-        ['app://search.gaiamobile.org/manifest.webapp']
-      ];
-
       var html = '';
-      manifests.forEach((tuple, index) => {
+      iconConfig.forEach((tuple, index) => {
         var manifest = tuple[0];
         var entryPoint = tuple[1];
 
@@ -70,10 +72,85 @@
       });
     },
 
+    /**
+     * Brings up a menu to replace an icon in the SHB menu.
+     */
+    customizeIcon: function(element) {
+      var select = document.createElement('select');
+      var thisSlot = element.id.match(/quick-app-([0-9]+)/)[1];
+      var entryPoint = element.dataset && element.dataset.entryPoint;
+
+      function renderOption(manifestUrl, descriptor, entryPoint) {
+        var option = document.createElement('option');
+        option.text = descriptor.name;
+        option.value = manifestUrl + (entryPoint ? ',' + entryPoint : '');
+
+        // Select the option if the manifestUrl matches, and entryPoint if we have one.
+        if (manifestUrl === iconConfig[thisSlot - 1][0] && (!entryPoint || entryPoint == iconConfig[thisSlot - 1][1])) {
+          option.selected = true;
+        }
+        select.appendChild(option);
+      }
+
+      for (var manifestUrl in applications.installedApps) {
+        var app = applications.installedApps[manifestUrl];
+        var manifest = app.manifest || app.updateManifest;
+
+        if (manifest.entry_points) {
+          for (var eachPoint in manifest.entry_points) {
+            renderOption(manifestUrl, manifest.entry_points[eachPoint], eachPoint);
+          }
+        } else {
+          renderOption(manifestUrl, manifest);
+        }
+      }
+
+      document.body.appendChild(select);
+      select.focus();
+
+      var onBlur = () => {
+        cleanup();
+      };
+
+      var onChange = e => {
+        // This will split the url into an array of manifestUrl, entryPoint.
+        // If an entrypoint does not exist, it will just be a one-object array.
+        var newDefinition = select.value.split(',');
+
+        iconConfig[thisSlot - 1] = newDefinition;
+        this.render();
+        cleanup();
+      };
+
+      function cleanup() {
+        select.removeEventListener('blur', onBlur);
+        select.removeEventListener('change', onChange);
+        document.body.removeChild(select);
+      }
+
+      select.addEventListener('blur', onBlur);
+      select.addEventListener('change', onChange);
+    },
+
     handleEvent: function(e) {
 
       switch (e.type) {
+        case 'touchstart':
+          this.customizationPress = setTimeout(() => {
+            this.customizeIcon(e.target);
+          }, 800);
+          break;
+
+        case 'touchend':
+          clearTimeout(this.customizationPress);
+          this.customizationPress = null;
+          break;
+
         case 'click':
+          // Reset long-press logic to avoid customization screen.
+          clearTimeout(this.customizationPress);
+          this.customizationPress = null;
+
           var icon = this.map.get(e.target);
           if (icon && e.target.dataset && e.target.dataset.entryPoint) {
             icon.launch(e.target.dataset.entryPoint);
